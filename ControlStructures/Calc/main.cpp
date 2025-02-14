6f10005543e22ea866f3c51fa7a13b3a076b2905
User <User@DESKTOP-0TCDSR6>
2025-02-14 21:46:32 +0300

    Loops

---
+++ ControlStructures/Calc/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define CALC_1
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	/*int x, y, z;
+	cin >> x >> y >> z;
+	cout << x << "\t" << y << "\t" << z << endl;*/
+
+#ifdef CALC_1
+	double a, b;	//числа, введенные с клавиатуры
+	char s;			//Sign - знак операции
+	cout << "Введите арифметическое выражение: ";
+	cin >> a >> s >> b;
+	if (s == '+') cout << a << " + " << b << " = " << a + b << endl;
+	else if (s == '-') cout << a << " - " << b << " = " << a - b << endl;
+	else if (s == '*') cout << a << " * " << b << " = " << a * b << endl;
+	else if (s == '/')
+	{
+		if (b == 0)cout << "Error: Division by Zero" << endl;
+		else cout << a << " / " << b << " = " << a / b << endl;
+	}
+	else cout << "Error: No operation" << endl;
+#endif // CALC_1
+
+	double a, b;
+	char s;
+	cout << "Введите арифметические выражение: "; cin >> a >> s >> b;
+	switch (s)
+	{
+	case '+':cout << a << " + " << b << " = " << a + b << endl; break;
+	case '-':cout << a << " - " << b << " = " << a - b << endl; break;
+	case '*':cout << a << " * " << b << " = " << a * b << endl; break;
+	case '/':cout << a << " / " << b << " = " << a / b << endl; break;
+	default: cout << "Error: No operation" << endl;
+	}
+}
--- ControlStructures/Loops/main.cpp
+++ ControlStructures/Loops/main.cpp
@@ -1,10 +1,30 @@
 ﻿#include<iostream>
 using namespace std;
 
+//#define WHILE_1
+#define WHILE_2
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Loop" << endl;
+
+#if defined WHILE_1
+	int i = 0;		//Счетчик цикла
+	int n = 5;		//Количество итераций
+	while (i < n)
+	{
+		cout << i << ". Loops " << endl;
+		i++;
+	}
+#endif // WHILE_1
+
+	double n;
+	cout << "Введите количество итераций: "; cin >> n;
+	while (n--)
+	{
+		cout << n << "\t";
+	}
+	cout << endl;
 }
 
 /*
