a868dea5a9a88a6f9f704c4f9d5a04147ca74dc9
User <User@DESKTOP-0TCDSR6>
2025-02-21 22:20:27 +0300

    Shooter DONE

--- ControlStructures/Palindrome/main.cpp
+++ ControlStructures/Palindrome/main.cpp
@@ -1,9 +1,17 @@
 ﻿#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define PALINDROME
+#define TICKET
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef PALINDROME
 	int number;
 	cout << "Введите число: "; cin >> number;
 	int buffer = number;	//Создаем копию введенного числа, поскольку данные введенные пользователем
@@ -20,4 +28,27 @@ void main()
 
 	if (reverse == number)cout << "Это палиндром" << endl;
 	else cout << "Это простое число" << endl;
+#endif // PALINDROME
+
+	int number;	//номер автобусного билета
+	cout << "Введите номер автобусного билета: "; cin >> number;
+	int sum_1 = 0;	//сумма трех младших разрядов
+	int sum_2 = 0;	//сумма трех старших разрядов
+
+	while (number > 1000)
+	{
+		sum_1 += number % 10;
+		number /= 10;
+	}
+	while (number > 0)
+	{
+		sum_2 += number % 10;
+		number /= 10;
+	}
+	cout << sum_1 << endl;
+	cout << sum_2 << endl;
+	if (sum_1 == sum_2)
+		cout << "Вам повезет ;-)" << endl;
+	else
+		cout << "Вам повезет, только в другой раз))" << endl;
 }
---
+++ ControlStructures/Shooter/main.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+#define Enter		13
+#define Escape		27
+
+#define UpArrow		72
+#define DownArrow	80
+#define LeftArrow	75
+#define RightArrow	77
+
+//#define SHOOTER_IF
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Shooter" << endl;
+
+#ifdef SHOOTER_IF
+	char key;
+	do
+	{
+		key = _getch();
+		//cout << (int)key << "\t" << key << endl;
+		if (key == 'w' || key == 'W' || key == UpArrow)
+		{
+			cout << "Вперед" << endl;
+		}
+		else if (key == 's' || key == 'S' || key == DownArrow)
+		{
+			cout << "Назад" << endl;
+		}
+		else if (key == 'a' || key == 'A' || key == LeftArrow)
+		{
+			cout << "Влево" << endl;
+		}
+		else if (key == 'd' || key == 'D' || key == RightArrow)
+		{
+			cout << "Вправо" << endl;
+		}
+		else if (key == ' ')
+		{
+			cout << "Прыжок" << endl;
+		}
+		else if (key == Enter)
+		{
+			cout << "Огонь" << endl;
+		}
+		else if (key == Escape)
+		{
+			cout << "Выход" << endl;
+		}
+		else
+		{
+			if (key != -32)cout << "Error" << endl;
+		}
+	} while (key != Escape);
+#endif // SHOOTER_IF
+
+	char key;
+	do
+	{
+		key = _getch();
+		//cout << (int)key << "\t" << key << endl;
+		switch (key)
+		{
+		case UpArrow:
+		case 'W':
+		case 'w':	cout << "Вперед" << endl;	break;
+		case DownArrow:
+		case 'S':
+		case 's':	cout << "Назад" << endl;	break;
+		case LeftArrow:
+		case 'A':
+		case 'a':	cout << "Влево" << endl;	break;
+		case RightArrow:
+		case 'D':
+		case 'd':	cout << "Вправо" << endl;	break;
+
+		case ' ':	cout << "Прыжок" << endl;	break;
+		case  Enter:cout << "Огонь" << endl;	break;
+		case Escape:
+		case -32:break;
+		default:	cout << "Error" << endl;
+		}
+	} while (key != Escape);
+}
