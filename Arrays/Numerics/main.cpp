5ad718cea4e301d35fba4a706ca91076ec638236
User <User@DESKTOP-QHG18FL>
2025-04-25 20:48:53 +0300

    Numerics

---
+++ Arrays/Numerics/main.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+
+//#define BINARY
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	for (int i = 0; i < 256; i++)
+	{
+		cout << i << "\t" << (char)i << endl;
+	}
+
+	int decimal;	//эта переменная будет хранить десятичное число, введенное пользователем
+	cout << "Введите десятичное число: "; cin >> decimal;
+
+#ifdef BINARY
+	const int n = 32;	//максимально-возможная разрядность двоичного числа
+	bool binary[n] = {};		//этот массив будет хранить разряды двоичного числа
+	int i = 0;
+	for (; decimal; decimal >>= 1)
+		binary[i++] = decimal & 1;
+
+	for (--i; i >= 0; i--)
+	{
+		cout << binary[i];
+		if (i % 8 == 0)cout << " ";
+		if (i % 4 == 0)cout << " ";
+	}
+	cout << endl;
+#endif // BINARY
+
+	const int n = 8;
+	char hex[n] = {};
+	int i = 0;
+	for (; decimal; decimal >>= 4)
+	{
+		hex[i] = decimal & 15;
+		hex[i++] += hex[i] < 10 ? 48 : 55;
+	}
+
+	for (--i; i >= 0; i--)
+		cout << hex[i];
+		//cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+	cout << endl;
+	}
