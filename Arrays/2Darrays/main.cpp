0104343d6eb702a544c78fac73e81a0af2ac9298
User <User@DESKTOP-QHG18FL>
2025-04-25 22:08:04 +0300

    2D Sort DONE

--- Arrays/2Darrays/main.cpp
+++ Arrays/2Darrays/main.cpp
@@ -1,4 +1,4 @@
-//2Darrays
+﻿//2Darrays
 #include<iostream>
 using namespace std;
 
@@ -8,12 +8,21 @@ void main()
 	cout << "2Darrays" << endl;
 	const int ROWS = 5;
 	const int COLS = 4;
-	int arr[ROWS][COLS] = 
+	int arr[ROWS][COLS]/* =
 	{
 		{ 1, 2, 3 },
 		{ 4, 5, 6 },
 		{ 7, 8, 9 },
-	};
+	}*/;
+	//Заполнение слечайными числами:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+	//Вывод исходного массива на экран:
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
@@ -22,4 +31,39 @@ void main()
 		}
 		cout << endl;
 	}
+	cout << endl;
+
+	//Сортировка массива:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				//for (int l = k == i ? j + 1 : 0; l < COLS; l++)
+				for (int l = k > i ? 0 : j + 1; l < COLS; l++)
+				{
+					//arr[i][j] - выбранный элемент
+					//arr[k][l] - перебираемый элемент
+					if (arr[k][l] < arr[i][j])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}
+				}
+			}
+		}
+	}
+
+	//Вывод отсортированного массива на экран:
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
