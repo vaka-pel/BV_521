66f60df9791360e30bde5213c69a2938b1d21c5e
User <User@DESKTOP-QHG18FL>
2025-04-25 23:05:44 +0300

    Unique2 DONE

---
+++ Arrays/Unique2/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int ROWS = 3;
+	const int COLS = 4;
+	int arr[ROWS][COLS];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			bool unique;
+			do
+			{
+				arr[i][j] = rand() % (ROWS*COLS);
+				unique = true;
+				for (int k = 0; k <= i; k++)
+				{
+					//for (int l = 0; l < (k < i ? COLS : j); l++)
+					for (int l = 0; l < (k == i ? j : COLS); l++)
+					{
+						if (arr[i][j] == arr[k][l])
+						{
+							unique = false;
+							break;
+						}
+					}
+					if (!unique)break;
+				}
+			} while (!unique);
+		}
+	}
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+}
