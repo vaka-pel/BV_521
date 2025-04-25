515d07969fd10144777485165644c47b99dc6d34
User <User@DESKTOP-QHG18FL>
2025-04-25 21:05:45 +0300

    Unique DONE

---
+++ Arrays/Unique/main.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		bool unique;
+		do
+		{
+			arr[i] = rand() % 10;
+			unique = true;	//предположим, что сгенерировалось уникальное случайное число,
+			//Но это нужно проверить:
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;	//ключевое слово 'break' прерывает текущую итерацию и все последующие итерации.
+				}
+			}
+		} while (!unique);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
