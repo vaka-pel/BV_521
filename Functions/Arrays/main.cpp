7d63ad14782db0be23b5c54a1061d0c2d6531cfc
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-05-21 20:59:04 +0300

    Int Functions DONE

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -12,28 +12,48 @@ void Print(double arr[], const int n);
 void Sort(int arr[], const int n);
 void Sort(double arr[], const int n);
 
+
 int Sum(int arr[], const int n);
+
+double Avg(int arr[], const int n);
+
+int MinValueIn(int arr[], const int n);
+
+int MaxValueIn(int arr[], const int n);
+
+void ShiftLeft(int arr[], const int n, const int number_of_shifts);
+void ShiftRight(int arr[], const int n, const int number_of_shifts);
+
 
 //60 строк кода
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 5;
-	int arr[n];
+	const int n = 10;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	FillRand(arr, n);
+	//FillRand(arr, n);
 	Print(arr, n);
 	Sort(arr, n);
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение элементов массива: " << MinValueIn(arr, n) << endl;
+	cout << "Максимальное значение элементов массива: " << MaxValueIn(arr, n) << endl;
+	
+
+	int number_of_shifts;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	ShiftRight(arr, n, number_of_shifts);
+	Print(arr, n);
 
 	const int SIZE = 8;
 	double brr[SIZE];
 	FillRand(brr, SIZE);
-	Print(brr, SIZE);
+	//Print(brr, SIZE);
 	Sort(brr, SIZE);
-	Print(brr, SIZE);
+	//Print(brr, SIZE);
 }
 
 void FillRand(int arr[], const int n, int minRand, int maxRand)
@@ -109,4 +129,43 @@ int Sum(int arr[], const int n)
 		sum += arr[i];
 	}
 	return sum;
+}
+double Avg(int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+int MinValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+int MaxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+void ShiftLeft(int arr[], const int n, const int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+
+		}
+		arr[n - 1] = buffer;
+	}
+}
+void ShiftRight(int arr[], const int n, const int number_of_shifts)
+{
+	ShiftLeft(arr, n, n - number_of_shifts);
 }
