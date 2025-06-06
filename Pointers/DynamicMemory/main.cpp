7ce2e86aaed89eeae0a1a0fa2e5c0a2b6d877691
User <User@DESKTOP-QHG18FL>
2025-06-06 20:45:35 +0300

    Dynamic arrays start

---
+++ Pointers/DynamicMemory/main.cpp
@@ -0,0 +1,40 @@
+﻿//DynamicMemory
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];	//Объявление динамического массива
+	//short  - short
+	//short* - Указатель на 'short';
+	//int  - 'int';
+	//int* - Указатель на 'int';
+	//double  - 'double';
+	//double* - Указатель на 'double';
+
+	FillRand(arr, n);
+	Print(arr, n);
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;	//Через арифметику указателей и оператор разыменования
+	}
+}
+void Print(int arr[], const int n)
+{
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";		//Через оператор индексирования (Subscript operator)
+	}
+	cout << endl;
+}
---
+++ Pointers/References/main.cpp
@@ -0,0 +1,15 @@
+﻿//References
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int& ra = a;	//Компилятор сам, неявно берет адрес переменной 'a', и записывает его в ссылку 'ra';
+	ra += 3;
+	cout << a << endl;
+	//В языке 'C' ссылок никогда небыло.
+	cout << &a << endl;
+	cout << &ra << endl;
+}
